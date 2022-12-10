f84678314f3fd3c6ac78a3a865f49bce4633a0d7
Dmitrandir <dim9107@mail.ru>
2022-12-11 02:01:50 +0300

    Constraints and action added.

--- SwitchApp/SwitchViewController.swift
+++ SwitchApp/SwitchViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 final class SwitchViewController: UIViewController {
     
     //MARK: - Private property
-    private var colorLabel = UILabel()
+    private var colorView = UIView()
     private var onOffLabel = UILabel()
     private var selectorSwitch = UISwitch()
     
@@ -21,39 +21,99 @@ final class SwitchViewController: UIViewController {
         super.viewDidLoad()
         setupView()
     }
-
+    
+    //MARK: - Actions
+    @objc
+    private func changeColorView() {
+        
+        if selectorSwitch.isOn {
+            colorView.backgroundColor = .systemRed
+            onOffLabel.text = "On"
+        } else {
+            colorView.backgroundColor = .systemGreen
+            onOffLabel.text = "Off"
+        }
+    }
 }
 //MARK: - Setting View
 extension SwitchViewController {
     func setupView() {
-        view.backgroundColor = .gray
+        view.backgroundColor = .systemMint
+        
+        addSubViews()
+        addActions()
+        
+        setupColorView()
+        setupOnOffLabel()
+        
+        setupLayout()
     }
 }
 
 //MARK: - Setting
 extension SwitchViewController {
     func addSubViews() {
-        view.addSubview(colorLabel)
+        view.addSubview(colorView)
         view.addSubview(onOffLabel)
         view.addSubview(selectorSwitch)
     }
     
-    func setupColorLabel() {
-        colorLabel.backgroundColor = .green
-        colorLabel.layer.cornerRadius = 15
+    func addActions() {
+        selectorSwitch.addTarget(
+            self,
+            action: #selector(changeColorView),
+            for: .touchUpInside)
+    }
+    
+    func setupColorView() {
+        colorView.backgroundColor = .systemGreen
+        colorView.layer.cornerRadius = 20
     }
     
     func setupOnOffLabel() {
         onOffLabel.text = "Off"
-        onOffLabel.backgroundColor = .gray
+        onOffLabel.font = UIFont.systemFont(ofSize: 32)
+        onOffLabel.backgroundColor = .systemMint
     }
     
-    func setupSelectorSwitch() {
-        
-    }
 }
 
 //MARK: - Layout
 extension SwitchViewController {
+    func setupLayout() {
+        
+        [colorView, onOffLabel, selectorSwitch].forEach { view in
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+        
+            colorView.topAnchor.constraint(equalTo: view.topAnchor,
+                                           constant: 150),
+            colorView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            colorView.widthAnchor.constraint(equalToConstant: 300),
+            colorView.heightAnchor.constraint(equalToConstant: 150),
+            
+            onOffLabel.topAnchor.constraint(equalTo: colorView.bottomAnchor, constant: 100),
+            onOffLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            selectorSwitch.topAnchor.constraint(equalTo: onOffLabel.bottomAnchor, constant: 20),
+            selectorSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+        
+       /*
+        colorView.topAnchor.constraint(equalTo: view.topAnchor,
+                                       constant: 150).isActive = true
+        colorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        colorView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        colorView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        onOffLabel.topAnchor.constraint(equalTo: colorView.bottomAnchor, constant: 100).isActive = true
+        onOffLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        selectorSwitch.topAnchor.constraint(equalTo: onOffLabel.bottomAnchor, constant: 20).isActive = true
+        selectorSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        */
+    }
     
 }
